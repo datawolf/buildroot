28ba9c4617f9bfa65fa41e550c77c8000b9ffed3
Wang Long <wanglong@laoqinren.net>
2016-07-05 14:29:19 +0000

    add lxcfs package
    Signed-off-by: Wang Long <wanglong@laoqinren.net>

---
+++ package/lxcfs/lxcfs.mk
@@ -0,0 +1,33 @@
+################################################################################
+#
+# lxcfs
+#
+################################################################################
+
+LXCFS_VERSION = lxcfs-0.16
+LXCFS_SITE = $(call github,lxc,lxcfs,$(LXCFS_VERSION))
+LXCFS_LICENSE = BSD-2
+LXCFS_LICENSE_FILES = COPYING
+LXCFS_DEPENDENCIES = libfuse
+
+
+define BOOTSTRAP
+	$(TARGET_MAKE_ENV)  cd $(@D); ./bootstrap.sh; cd -
+endef
+
+LXCFS_PRE_CONFIGURE_HOOKS += BOOTSTRAP
+define LXCFS_CONFIGURE_CMDS
+	$(TARGET_MAKE_ENV) cd $(@D);  ./configure; cd -
+endef
+
+define LXCFS_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define LXCFS_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) install
+endef
+
+
+$(eval $(generic-package))
+
